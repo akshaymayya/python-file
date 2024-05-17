6f33d48dd001fa5bb47e99051dfb42c3eea73506
akshaymayya <167389073+akshaymayya@users.noreply.github.com>
2024-05-17 20:26:03 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+name = "akshay" 
+age = 18
+price = 2.99
+age2 = age
+print("my name is :",name)
+print("my age is :",age)
+print("price :",price)
+
+prin
---
+++ am.py
@@ -0,0 +1,3 @@
+a=int(input("enter A variable"))
+b=int(input("enter B variable"))
+print(a>=b)
